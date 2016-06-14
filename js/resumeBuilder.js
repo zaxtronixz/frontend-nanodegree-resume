5050f0ad32e343ec58ca5e6d37c53b4cf0d981f5
zaxtronixz <zax20yau@gmail.com>
2016-06-14 05:57:08 +0100

    The resumenuilder.js has been updated to include name in header

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,31 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+ //$("#main").append(["Zakariya'u  Tanimu Muhammad "]);
+ var awesomeThoughts;
+ awesomeThoughts = "I am Zakariya and I am AWESOME";
+
+ console.log(awesomeThoughts);
+
+var email = "zax20yau@gmail.com";
+var newEmail = email.replace("gmail","udacity");
+
+console.log(email);
+console.log(newEmail);
+
+
+var funThoughts = awesomeThoughts.replace("AWESOME","FUN");
+
+$("#main").append(funThoughts);
+
+var formattedName;
+var name = "Zakariyau Tanimu Muhammad";
+var formattedRole;
+var role = "Software Engineer";
+
+// The Two HTMLheader..... are variables referenced from helper.js file
+formattedName = HTMLheaderName.replace("%data%",name);
+formattedRole = HTMLheaderRole.replace("%data%",role);
+
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
